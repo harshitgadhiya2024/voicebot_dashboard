779d949f0446661e992bd5fa3979a48370ef354e
harshittrsthealth <150043423+harshittrsthealth@users.noreply.github.com>
2024-06-11 22:05:57 +0530

    new changes

--- main.py
+++ main.py
@@ -536,15 +536,9 @@ def inputnodeapi():
         uid = request.args.get("uid", "")
         node_id = request.args.get("node_id", "")
         timestamp = request.args.get("timestamp", "")
-        clid = request.args.get("clid", "")
-        app.logger.debug(f"clid1: {clid}")
-        input = request.args.get("input", "")
         response_data = json.loads(request.data)
         clid = response_data.get("clid", "")
         app.logger.debug(f"clid2: {clid}")
-        if request.method=="POST":
-            clid = request.form.get("clid", "")
-            app.logger.debug(f"clid3: {clid}")
 
         smart_voicecall_details=app.config["smart_voicecall_details"]
         app.logger.debug(smart_voicecall_details)
